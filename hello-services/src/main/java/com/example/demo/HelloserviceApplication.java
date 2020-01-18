046455495a05178b5af2073b859b251eaea82064
saroj <sarojlike@gmail.com>
2020-01-18 22:48:00 +0530

    hello-services - simply publishes the hello message Signed-off-by: saroj <sarojlike@gmail.com>

---
+++ hello-services/src/main/java/com/example/demo/HelloserviceApplication.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloserviceApplication {
+	
+	@Value("${hello.message}")
+	private String message;
+	
+	
+	@Value("${hi.message2}") 
+	private String message2;
+	 
+	
+	@RequestMapping("/hello")
+	public Map<String, String> hello() {
+		HashMap<String, String> response = new HashMap<String,String>(){{
+			this.put("status", "OK");
+			this.put("message", message);
+		}};
+		return response;
+	}
+	
+	
+	  @RequestMapping("/hi") 
+	  public Map<String, String> sayHi() { 
+		  HashMap<String,String> response = new HashMap<String,String>(){{ 
+				  this.put("status", "OK");	  
+				  this.put("message", message2); 
+			  }}; 
+		return response; 
+	}
+	 
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloserviceApplication.class, args);
+	}
+
+}
